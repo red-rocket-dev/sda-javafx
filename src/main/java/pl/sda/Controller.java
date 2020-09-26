ca505665f6b8cd18d279e098c124b6849136540e
Jakub <kubplon@gmail.com>
2020-09-26 13:02:21 +0200

    random triangles

--- src/main/java/pl/sda/Controller.java
+++ src/main/java/pl/sda/Controller.java
@@ -1,25 +1,19 @@
 package pl.sda;
 
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Window;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.Random;
 
 
 public class Controller {
@@ -30,52 +24,41 @@ public class Controller {
     @FXML
     private Button button;
 
-    public void initialize() throws IOException {
-        final GraphicsContext gc = canvas.getGraphicsContext2D();
+    public void initialize() {
 
-        gc.setFill(Color.BLACK);
-        double xPoints[] = {0, 0, 300};
-        double yPoints[] = {0, 500, 300};
-        gc.fillPolygon(xPoints, yPoints, 3);
     }
 
-    private void drawShapes(GraphicsContext gc) {
-        gc.setFill(Color.rgb(255, 0, 0));
-        gc.setStroke(Color.BLUE);
-
-        gc.fillRect(400,400,100,100);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                new double[]{210, 210, 240, 240}, 4);
+    private void drawTriangles(int count) {
+        Random random = new Random();
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+        for (int i = 0; i < count; i++) {
+            gc.setFill(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+            double[] xPoints = {random.nextInt(600), random.nextInt(600), random.nextInt(600)};
+            double[] yPoints = {random.nextInt(600), random.nextInt(600), random.nextInt(600)};
+            gc.fillPolygon(xPoints, yPoints, 3);
+        }
     }
 
     public void buttonClick(ActionEvent actionEvent) {
         Window owner = canvas.getScene().getWindow();
         TextInputDialog alert = new TextInputDialog();
-        alert.setTitle("Title");
+        alert.setTitle("Ile trojkatow?");
+        alert.setWidth(1000);
+        alert.setHeight(1000);
         alert.setHeaderText(null);
-        alert.setContentText("Hello world");
+        alert.setContentText("Wprowadz ilosc trojkatow");
         alert.initOwner(owner);
         final Optional<String> result = alert.showAndWait();
-        result.ifPresent(res -> System.out.println(res+"asdasd"));
+        result.ifPresent(res -> {
+            Integer desiredCount = Integer.valueOf(res);
+            drawTriangles(desiredCount);
+        });
+
+
     }
 
-
-    public void showMessageBtnAction(ActionEvent actionEvent) {
-        showMessageLabel.setText("Hello world!");
+    public void cleanBtnAction(ActionEvent actionEvent) {
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0,0,600,600);
     }
 }
